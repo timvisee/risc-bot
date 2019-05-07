8351526050b3daf7e87fc1f85f283fdd357f3745
timvisee <tim@visee.me>
2019-05-08 00:50:59 +0200

    Allow null values for chat titles

--- migrations/2018-07-14-112340_create_chats/up.sql
+++ migrations/2018-07-14-112340_create_chats/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE chat (
   telegram_id   BIGINT          PRIMARY KEY,
-  title         VARCHAR(255)    NOT NULL,
+  title         VARCHAR(255),
   created_at    DATETIME        NOT NULL
     DEFAULT CURRENT_TIMESTAMP,
   updated_at    DATETIME        NOT NULL
