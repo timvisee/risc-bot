204dad7163d49b8aee8bf83af65b1760c84de3d8
Bram Crielaard <bram@crielaard.co.uk>
2019-09-11 10:00:39 +0200

    Allow y sed in messages

--- src/msg/handler.rs
+++ src/msg/handler.rs
@@ -21,7 +21,7 @@ lazy_static! {
 
     /// A regex for matching messages that contain sed syntax.
     static ref SED_REGEX: Regex = Regex::new(
-        r"^\s*(s/.*/.*/[a-zA-Z0-9]*)\s*$",
+        r"^\s*([sy]/.*/.*/[a-zA-Z0-9]*)\s*$",
     ).expect("failed to compile SED_REGEX");
 }
 
